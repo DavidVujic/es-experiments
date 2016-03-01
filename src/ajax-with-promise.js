ee665b1d6690db161aaa0efa1b9d2af6c3da06e5
David Vujic <david@vujic.se>
2016-03-01 20:54:41 +0100

    refactor: ajax with promise imports and consumes the callback version

--- src/ajax-with-promise.js
+++ src/ajax-with-promise.js
@@ -1,25 +1,9 @@
-let get = (url) => {
+import { default as ajaxWithCallback } from 'ajax-with-callback';
 
+function get(url) {
 	return new Promise((resolve, reject) => {
-
-		let request = new XMLHttpRequest();
-		request.open('GET', url, true);
-
-		request.onload = function () {
-			if (this.status >= 200 && this.status < 400) {
-				resolve(this.response);
-			} else {
-				reject('We reached our target server, but it returned an error');
-			}
-
-		};
-
-		request.onerror = function () {
-			reject('There was a connection error of some sort');
-		};
-
-		request.send();
+		ajaxWithCallback(url, resolve, reject);
 	});
-};
+}
 
 export default get;
